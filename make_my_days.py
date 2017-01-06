65c902b326b5f82d16bfed464443537279f83b74
noticemesenpy <sgipeople@yahoo.com>
2017-01-06 18:56:42 +0100

    made my days :)

---
+++ make_my_days.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""
+filename.py
+
+copyright 2017 noticemesen.py
+
+gitlab: gitlab.com/noticemesenpy
+github: github.com/noticemesenpy
+
+********************************************************************************
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+********************************************************************************
+"""
+
+def main():
+    """the main function"""
+    with open("make_my_days.sh", "w") as bcomm:
+        bcomm.write("#!/bin/bash\n\n")
+        bcomm.write("mkdir day{01..24}\n\n")
+        bcomm.write("vim -o ")
+        for i in range(24):
+            bcomm.write("day" + str(i + 1).zfill(2) + "/day" + str(i + 1).zfill(2) + ".py ")
+
+
+if __name__ == "__main__":
+    main()
---
+++ make_my_days.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+mkdir day{01..24}
+
+vim -o day01/day01.py day02/day02.py day03/day03.py day04/day04.py day05/day05.py day06/day06.py day07/day07.py day08/day08.py day09/day09.py day10/day10.py day11/day11.py day12/day12.py day13/day13.py day14/day14.py day15/day15.py day16/day16.py day17/day17.py day18/day18.py day19/day19.py day20/day20.py day21/day21.py day22/day22.py day23/day23.py day24/day24.py 
